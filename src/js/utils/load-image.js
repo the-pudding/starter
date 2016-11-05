33905d46928dbaa51b35b7319c781d9fdb99b32e
russellgoldenberg <russellgoldenberg@gmail.com>
2016-11-05 18:08:29 -0400

    loadimage

--- src/js/utils/load-image.js
+++ src/js/utils/load-image.js
@@ -1,11 +1,11 @@
 const loadImage = (url, cb) => {
     const img = new Image()
     
-    img.onload = () => cb(null)
+    img.onload = () => cb(null, img)
 
     img.onerror = () => cb('error loading image: ' + url)
 
     img.src = url
 }
 
-export default loadImage 
+export default loadImage
