3b52cd3cb3d983d89574ebea04f9168726fdba06
russellgoldenberg <russellgoldenberg@gmail.com>
2016-11-23 17:09:15 -0500

    new font methods

--- src/js/utils/load-font.js
+++ src/js/utils/load-font.js
@@ -1,45 +1,27 @@
 import 'promis'
 import FontFaceObserver from 'fontfaceobserver'
-import { select, addClass } from './dom'
+import { addClass } from './dom'
 
-window.fontsLoaded = []
+const htmlEl = document.documentElement
+const TIMEOUT = 5000
 
-const createStylesheet = () => {
-	const style = document.createElement('style')
-	document.head.appendChild(style)
-	return style.sheet
-}
-
-const addFontRule = ({ font, sheet }) => {
-	// console.log(font)
-	const { weight, family } = font
+function addFont({ family, weight }) {
 	const name = family.toLowerCase().replace(/ /g, '-')
-	const suffix = weight.toString().charAt(0)
-	const className = `tk-${name}-${suffix}`
-	const rule = `
-		.${className} {
-			font-family: '${family}';
-			font-weight: ${weight};
-		}
-	`.trim()
-	
-	window.fontsLoaded.push(className)
-	sheet.insertRule(rule, 0)
+	const className = `loaded-${name}-${weight}`
+	addClass(htmlEl, className)
 }
 
-const handleError = err => console.error(err)
+function handleError(err) {
+	console.error(err)
+}
 
-const loadFont = fonts => {
-	const sheet = createStylesheet()
-	const el = document.documentElement
-	const timeout = 8000
-
+function loadFont(fonts) {
 	fonts.forEach(font => {
 		const { family, weight } = font
 		const fontObserver = new FontFaceObserver(family, { weight })
 		fontObserver
-			.load(null, timeout)
-			.then(() => addFontRule({ font, sheet }))
+			.load(null, TIMEOUT)
+			.then(() => addFont(font))
 			.catch(handleError)
 	})
 }
