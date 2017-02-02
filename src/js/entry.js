86d863e682d287231ab771ef3c3413576f5b61c3
russellgoldenberg <russellgoldenberg@gmail.com>
2017-02-02 13:40:13 -0500

    update entry

--- src/js/entry.js
+++ src/js/entry.js
@@ -7,12 +7,6 @@ const DEV_MODE = window.location.hostname.indexOf('localhost') > -1
 const bodyEl = $.select('body')
 let previousWidth = 0
 
-function logVersion() {
-	const el = $.select('html')
-	const v = el.getAttribute('data-version') 
-	console.log(`version: ${v.slice(5, v.length)}`)
-}
-
 function addMobileClass() {
 	const el = $.select('html')
 	if (isMobile.any()) $.addClass(el, 'is-mobile')
@@ -27,7 +21,6 @@ function handleResize() {
 }
 
 function init() {
-	logVersion()
 	addMobileClass()
 	window.addEventListener('resize', debounce(handleResize, 150))
 }
