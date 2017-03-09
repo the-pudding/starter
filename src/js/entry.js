e828158c126565d5f247c5f5408dd8e96a32601d
russellgoldenberg <russellgoldenberg@gmail.com>
2017-03-09 17:14:55 -0500

    graphic starter

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,27 +1,22 @@
 import debounce from 'lodash.debounce'
-import * as $ from './utils/dom'
+import { select, addClass } from './utils/dom'
 import isMobile from './utils/is-mobile'
 
-const DEV_MODE = window.location.hostname.indexOf('localhost') > -1
-
-const bodyEl = $.select('body')
+const bodyEl = select('body')
 let previousWidth = 0
-
-function addMobileClass() {
-	const el = $.select('html')
-	if (isMobile.any()) $.addClass(el, 'is-mobile')
-}
 
 function handleResize() {
 	const width = bodyEl.offsetWidth
 	if (previousWidth !== width) {
-		// resize here
 		previousWidth = width
+		// resize here
 	}
 }
 
 function init() {
-	addMobileClass()
+	// add mobile class to body tag
+	if (isMobile.any()) addClass(bodyEl, 'is-mobile')
+	// setup resize event
 	window.addEventListener('resize', debounce(handleResize, 150))
 }
 
---
+++ src/js/graphic.js
@@ -0,0 +1,11 @@
+import * as d3 from 'd3'
+
+function resize() {
+
+}
+
+function init() {
+	console.log('Make something awesome!')
+}
+
+export default { init, resize }
