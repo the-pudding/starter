a45be4aaa09730947bc5ca5dd987d2f29821b868
russellgoldenberg <russellgoldenberg@gmail.com>
2017-04-13 11:27:05 -0400

    tracker fix

--- src/js/utils/tracker.js
+++ src/js/utils/tracker.js
@@ -7,7 +7,8 @@ function check({ category, action, once }) {
 	if (once) {
 		const key = `${category.replace(/\W+/g, '')}${action.replace(/\W+/g, '')}`
 		if (fired[key]) return false
-		return fired[key] = true
+		fired[key] = true
+		return true
 	}
 	return true
 }
