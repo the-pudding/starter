0e2ec711287c43bff9aff63d9632019e80cf1745
russellgoldenberg <russellgoldenberg@gmail.com>
2017-04-30 19:38:02 -0400

    urlparam

---
+++ src/js/utils/get-url-parameter.js
@@ -0,0 +1,8 @@
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+export default getUrlParameter
