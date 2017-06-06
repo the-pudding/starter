aa3eea4df1a02ec57ff4943e4ffda87ccf57f68e
russellgoldenberg <russellgoldenberg@gmail.com>
2017-06-06 08:28:39 -0400

    use d3

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,14 +1,13 @@
 // D3 is included by globally by default
 import debounce from 'lodash.debounce'
-import { select, addClass } from './utils/dom'
 import isMobile from './utils/is-mobile'
 import graphic from './graphic'
 
-const bodyEl = select('body')
+const bodySel = d3.select('body')
 let previousWidth = 0
 
 function resize() {
-	const width = bodyEl.offsetWidth
+	const width = bodySel.node().offsetWidth
 	if (previousWidth !== width) {
 		previousWidth = width
 		graphic.resize()
@@ -17,7 +16,7 @@ function resize() {
 
 function init() {
 	// add mobile class to body tag
-	if (isMobile.any()) addClass(bodyEl, 'is-mobile')
+	bodySel.classed('is-mobile', isMobile.any())
 	// setup resize event
 	window.addEventListener('resize', debounce(resize, 150))
 	// kick off graphic code
