3227ad83c542307d736d13db34254035580ce6ff
russellgoldenberg <russellgoldenberg@gmail.com>
2017-10-04 09:52:51 -0400

    url parameters

--- src/js/utils/get-url-parameter.js
+++
@@ -1,8 +0,0 @@
-function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-export default getUrlParameter
