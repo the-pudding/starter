0d592d917e988da47b14e7dc887fe95a03ca0916
russellgoldenberg <russellgoldenberg@gmail.com>
2018-04-23 09:54:23 -0400

    resize

--- src/js/pudding-chart/base.js
+++ src/js/pudding-chart/base.js
@@ -12,30 +12,55 @@ d3.selection.prototype.puddingChartName = function init(options) {
 		const $sel = d3.select(el);
 		let data = $sel.datum();
 		// dimension stuff
-		const outerW = 0;
-		const outerH = 0;
-		const innerW = 0;
-		const innerH = 0;
+		let width = 0;
+		let height = 0;
 		const marginTop = 0;
 		const marginBottom = 0;
 		const marginLeft = 0;
 		const marginRight = 0;
 
+		// scales
+		const scaleX = null;
+		const scaleY = null;
+
+		// dom elements
+		let $svg = null;
+		let $axis = null;
+		let $vis = null;
+
+		// helper functions
+
 		const Chart = {
 			// called once at start
 			init() {
-				console.log('init');
+				$svg = $sel.append('svg.pudding-chart');
+				const $g = $svg.append('g');
+
+				// offset chart for margins
+				$g.at('transform', `translate(${marginLeft}, ${marginTop})`);
+
+				// create axis
+				$axis = $svg.append('g.g-axis');
+
+				// setup viz group
+				$vis = $g.append('g.g-vis');
+
 				Chart.resize();
 				Chart.render();
 			},
 			// on resize, update new dimensions
 			resize() {
-				console.log('resize');
+				// defaults to grabbing dimensions from container element
+				width = $sel.node().offsetWidth - marginLeft - marginRight;
+				height = $sel.node().offsetHeight - marginTop - marginBottom;
+				$svg.at({
+					width: width + marginLeft + marginRight,
+					height: height + marginTop + marginBottom
+				});
 				return Chart;
 			},
 			// update scales and render chart
 			render() {
-				console.log('render');
 				return Chart;
 			},
 			// get / set data
