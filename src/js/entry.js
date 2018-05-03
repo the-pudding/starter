89bcf378299fd1d9fd4c4c086b4186e7c20cd866
russellgoldenberg <russellgoldenberg@gmail.com>
2018-05-03 13:59:32 -0400

    syntax entry

--- src/js/entry.js
+++ src/js/entry.js
@@ -3,11 +3,13 @@ import debounce from 'lodash.debounce';
 import isMobile from './utils/is-mobile';
 import graphic from './graphic';
 
-const bodySel = d3.select('body');
+const $body = d3.select('body');
 let previousWidth = 0;
 
 function resize() {
-	const width = bodySel.node().offsetWidth;
+	// only do resize on width changes, not height
+	// (remove the conditional if you want to trigger on height change)
+	const width = $body.node().offsetWidth;
 	if (previousWidth !== width) {
 		previousWidth = width;
 		graphic.resize();
@@ -16,7 +18,7 @@ function resize() {
 
 function init() {
 	// add mobile class to body tag
-	bodySel.classed('is-mobile', isMobile.any());
+	$body.classed('is-mobile', isMobile.any());
 	// setup resize event
 	window.addEventListener('resize', debounce(resize, 150));
 	// kick off graphic code
