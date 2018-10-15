8b243d980a605c630907f0f8de970e2ac195dbfd
russellgoldenberg <russellgoldenberg@gmail.com>
2018-10-15 14:47:39 -0400

    fix vars and google fetcher

--- gulp-tasks/fetch-google.js
+++ gulp-tasks/fetch-google.js
@@ -1,31 +1,30 @@
-const gulp = require('gulp')
-const archieml = require('archieml')
-const request = require('request')
-const fs = require('fs')
-const configPath = `${process.cwd()}/config.json`
-const config = JSON.parse(fs.readFileSync(configPath, 'utf8'))
-const google = config.google
+const gulp = require('gulp');
+const archieml = require('archieml');
+const request = require('request');
+const fs = require('fs');
 
-const url = `https://docs.google.com/document/d/${google.id}/export?format=txt`
+const configPath = `${process.cwd()}/config.json`;
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
-const makeRequest = (cb) => {
-	request(url, function(error, response, body) {
-		const parsed = archieml.load(body)
-		const str = JSON.stringify(parsed)
-		const basePath = `${process.cwd()}/template-data`
-		const file = `${basePath}/${(google.filename || 'copy')}.json`
+const makeRequest = (opt, cb) => {
+	const url = `https://docs.google.com/document/d/${opt.id}/export?format=txt`;
+	request(url, (error, response, body) => {
+		const parsed = archieml.load(body);
+		const str = JSON.stringify(parsed);
+		const basePath = `${process.cwd()}`;
+		const file = opt.filename || 'template-data/copy.json';
+		const filepath = `${basePath}/${file}`;
+		fs.writeFile(filepath, str, err => {
+			if (err) console.error(err);
+			cb();
+		});
+	});
+};
 
-		fs.writeFile(file, str, (err) => {
-			if (err) console.error(err)
-			cb()
-		})
-	})
-}
-
-gulp.task('fetch-google', (cb) => {
-	if (google.id) makeRequest(cb)
+gulp.task('fetch-google', cb => {
+	if (config.google.id) makeRequest(config.google, cb);
 	else {
-		console.error('No google doc')
-		cb()
+		console.error('No google doc');
+		cb();
 	}
-})
+});
