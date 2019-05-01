3bb803170a78be7d9c86c9edb946c5602856f849
russellgoldenberg <russellgoldenberg@gmail.com>
2019-05-01 15:29:37 -0400

    fix depudding

--- scripts/depudding.js
+++ scripts/depudding.js
@@ -3,75 +3,86 @@ const replace = require('replace-in-file');
 
 // basepath
 replace.sync({
-  files: "./scripts/html.js",
-  from: "https://pudding.cool/",
-  to: ",
+  files: './scripts/html.js',
+  from: 'https://pudding.cool/',
+  to: '',
 });
 
 // meta
 replace.sync({
-  files: "./data/meta.json",
-  from: "https://pudding.cool/",
-  to: "https://site.com/",
+  files: './data/meta.json',
+  from: /https:\/\/pudding\.cool/g,
+  to: 'https://site.com',
+});
+
+replace.sync({
+  files: './data/meta.json',
+  from: /The Pudding/g,
+  to: '',
+});
+
+replace.sync({
+  files: './data/meta.json',
+  from: '@puddingviz',
+  to: '',
 });
 
 // remove footer
 replace.sync({
-  files: "./src/html/partials/index.hbs",
-  from: "{{> base/footer }}",
-  to: ",
+  files: './src/html/index.hbs',
+  from: '{{> base/footer }}',
+  to: '',
 });
 
 // remove header
 replace.sync({
-  files: "./src/html/partials/index.hbs",
+  files: './src/html/index.hbs',
   from: "{{> base/header 'sticky' }}",
-  to: "",
+  to: '',
 });
 
 // main.js
 replace.sync({
-	files: "./src/js/main.js",
-	from: "import footer from './footer';",
-	to: "// import footer from './footer';",
+  files: './src/js/main.js',
+  from: "import footer from './footer';",
+  to: "// import footer from './footer';",
 });
 
 replace.sync({
-	files: "./src/js/main.js",
-	from: "setupStickyHeader();",
-	to: "// setupStickyHeader();",
+  files: './src/js/main.js',
+  from: 'setupStickyHeader();',
+  to: '// setupStickyHeader();',
 });
 
 replace.sync({
-	files: "./src/js/main.js",
-	from: "footer.init();",
-	to: "// footer.init();",
+  files: './src/js/main.js',
+  from: 'footer.init();',
+  to: '// footer.init();',
 });
-
 
 // variables.styl
 replace.sync({
-	files: "./src/css/base/variables.styl",
-	from: "$sans = $national, $sans-fb",
-	to: "$sans = $sans-fb",
+  files: './src/css/base/variables.styl',
+  from: '$sans = $national, $sans-fb',
+  to: '$sans = $sans-fb',
 });
 
 replace.sync({
-	files: "./src/css/base/variables.styl",
-	from: "$sans-display = $national-narrow, $sans-fb",
-	to: "$sans-display = $sans-fb",
+  files: './src/css/base/variables.styl',
+  from: '$sans-display = $national-narrow, $sans-fb',
+  to: '$sans-display = $sans-fb',
 });
 
 replace.sync({
-	files: "./src/css/base/variables.styl",
-	from: "$serif = $tiempos, $serif-fb",
-	to: "$serif = $serif-fb",
+  files: './src/css/base/variables.styl',
+  from: '$serif = $tiempos, $serif-fb',
+  to: '$serif = $serif-fb',
 });
 
 replace.sync({
-	files: "./src/css/base/variables.styl",
-	from: "$serif-display = $tiempos-hed, $serif-fb",
-	to: "$serif-display = $serif-fb",
+  files: './src/css/base/variables.styl',
+  from: '$serif-display = $tiempos-hed, $serif-fb',
+  to: '$serif-display = $serif-fb',
 });
 
 // remove fonts from assets
