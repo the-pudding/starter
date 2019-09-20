f203b5a2dc2fa23e179de01bd92aca2e3abc3c8b
russellgoldenberg <russellgoldenberg@gmail.com>
2019-09-20 15:03:56 -0400

    remove share tools

--- src/js/footer.js
+++ src/js/footer.js
@@ -79,28 +79,11 @@ function recircHTML() {
   d3.select('.pudding-footer .footer-recirc__articles').html(html);
 }
 
-function setupSocialJS() {
-  // facebook
-  (function(d, s, id) {
-    let js;
-
-    const fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7';
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, 'script', 'facebook-jssdk');
-
-  loadJS('https://platform.twitter.com/widgets.js');
-}
-
 function init() {
   loadStories(data => {
     storyData = data;
 
     recircHTML();
-    setupSocialJS();
   });
 }
 
