3efc27e5fa3c7e624e7e29e6ffef375b5ea1054f
russellgoldenberg <russellgoldenberg@gmail.com>
2019-11-12 12:28:21 -0500

    remove head and add new d3

--- src/js/head.js
+++
@@ -1,7 +0,0 @@
-import 'promise-polyfill/src/polyfill';
-import 'whatwg-fetch';
-import './polyfills/startsWith';
-import './polyfills/endsWith';
-import './polyfills/findIndex';
-import './polyfills/find';
-import './polyfills/includes';
