5f29cf4ef2482e77109fdecb02421d6367cdbdce
russellgoldenberg <russellgoldenberg@gmail.com>
2019-11-13 17:27:19 -0500

    csv sheet

--- scripts/fetch-sheet.js
+++ scripts/fetch-sheet.js
@@ -8,35 +8,33 @@ const CONFIG = JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf-8'));
 const { sheet } = CONFIG.google;
 
 const makeRequest = (opt, cb) => {
-  const base = 'https://docs.google.com/spreadsheets/u/1/d';
-  const url = `${base}/${opt.id}/export?format=csv&id=${opt.id}&gid=${opt.gid}`;
-  request(url, (error, response, body) => {
-    if (error) console.log(error);
-    else if (response) {
-      const data = dsv.csvParse(body);
-      const str = JSON.stringify(data);
-      const file = `${CWD}/${opt.filepath || 'data/sheet.json'}`;
-      fs.writeFile(file, str, err => {
-        if (err) console.error(err);
-        cb();
-      });
-    }
-  });
+	const base = 'https://docs.google.com/spreadsheets/u/1/d';
+	const url = `${base}/${opt.id}/export?format=csv&id=${opt.id}&gid=${opt.gid}`;
+	request(url, (error, response, body) => {
+		if (error) console.log(error);
+		else if (response) {
+			const file = `${CWD}/${opt.filepath || 'data/sheet.csv'}`;
+			fs.writeFile(file, body, err => {
+				if (err) console.error(err);
+				cb();
+			});
+		}
+	});
 };
 
 function init() {
-  let i = 0;
-  const next = () => {
-    const d = sheet[i];
-    if (d.id)
-      makeRequest(d, () => {
-        i += 1;
-        if (i < sheet.length) next();
-        else process.exit();
-      });
-  };
+	let i = 0;
+	const next = () => {
+		const d = sheet[i];
+		if (d.id)
+			makeRequest(d, () => {
+				i += 1;
+				if (i < sheet.length) next();
+				else process.exit();
+			});
+	};
 
-  next();
+	next();
 }
 
 init();
