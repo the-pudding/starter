3bb1ea71b576cc494067abd2749a3219f87ff983
russellgoldenberg <russellgoldenberg@gmail.com>
2020-06-03 12:54:18 -0400

    allow load image without cb

--- src/js/utils/load-image.js
+++ src/js/utils/load-image.js
@@ -5,10 +5,12 @@
  */
 
 function loadImage(url, cb) {
-	const img = new Image();
-	img.onload = () => cb(null, img);
-	img.onerror = () => cb(`error loading image: ${url}`);
-	img.src = url;
+  const img = new Image();
+  if (typeof cb === "function") {
+    img.onload = () => cb(null, img);
+    img.onerror = () => cb(`error loading image: ${url}`);
+  }
+  img.src = url;
 }
 
 export default loadImage;
