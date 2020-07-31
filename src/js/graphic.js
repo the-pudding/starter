1d7e1e0e249b7d14ecfad463cc6e0ba559c58fc8
russellgoldenberg <russellgoldenberg@gmail.com>
2020-07-31 09:58:53 -0400

    remove locate demo

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,10 +1,8 @@
 /* global d3 */
-import locate from './utils/locate';
 function resize() {}
 
 function init() {
   console.log('Make something awesome!');
-	locate().then(console.log).catch(console.log)
 }
 
 export default { init, resize };
